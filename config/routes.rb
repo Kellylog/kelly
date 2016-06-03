28cd9b6465707164ef0778ee89e9d4490e53e14a
kellylog <kellylogblog@gmail.com>
2016-06-03 06:56:06 +0000

    main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Kelly::Application.routes.draw do
+  root 'mainpage#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
